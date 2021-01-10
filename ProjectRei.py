37d38ca0b6e746370f1ae46cbcd4c74accb7de66
shoma444 <syamanou@physics.utoronto.ca>
2021-01-10 02:37:01 -0500

    Added del/edit buttons. Newly added class info now in main frame. Scroll bar added. Position of the class list needs fixing.

--- ProjectRei.py
+++ ProjectRei.py
@@ -12,12 +12,12 @@ import wx.lib.scrolledpanel as scrolled
 class MainMenu(wx.Frame):
     #main panel constructor
     def __init__(self):
-        screensize = (550,600)
+        screensize = (400,575)
         super(MainMenu, self).__init__(parent=None, title='Easy A',size=screensize)
         self.Bind(wx.EVT_ACTIVATE_APP, self.OnActivate)
         panel = wx.Panel(self)
-        #panelscroll = wx.lib.scrolledpanel.ScrolledPanel(self,-1, size=(-1,-1), pos=(-1,-1), style=wx.SIMPLE_BORDER)
-        #panelscroll.SetupScrolling()
+        self.panel = panel
+
         # Menu bar stuff
         self.menubar = wx.MenuBar()
         wx.MenuBar.MacSetCommonMenuBar(self.menubar)
@@ -32,58 +32,85 @@ class MainMenu(wx.Frame):
         
         # Main window layout
         menu_sizer = wx.BoxSizer(wx.VERTICAL)
-        
+        self.sizer = menu_sizer
         #self.text_ctrl = wx.TextCtrl(panel)
         #menu_sizer.AddSpacer(0)
         
         #top left design
         shoma_icon = wx.StaticBitmap(panel, bitmap=wx.Bitmap('./images/Easy_A_logo_mini.png'),style = wx.ALIGN_LEFT)
-        menu_sizer.Add(shoma_icon, 0, wx.RIGHT | wx.LEFT, 20)
+        self.sizer.Add(shoma_icon, 0, wx.RIGHT | wx.LEFT, 20)
         #menu_sizer.AddSpacer(0)
         Header_font = wx.Font(14, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
         Welcome = wx.StaticText(panel, -1, style = wx.ALIGN_LEFT)
         Welcome.SetLabel('Easy A\nMain Menu\n\n')
         Welcome.SetFont(Header_font)
-        menu_sizer.Add(Welcome, 0, wx.RIGHT | wx.LEFT, 20)
+        self.sizer.Add(Welcome, 0, wx.RIGHT | wx.LEFT, 20)
 
+        Header = wx.StaticText(panel, -1, style = wx.ALIGN_LEFT)
+        Header.SetFont(Header_font.Underlined())
+        Header.SetLabel(' Class\t Target Score  \tExam Date\n')
+        self.sizer.Add(Header, 0, wx.RIGHT | wx.LEFT, 20)
+
+        self.deleteoredit = []
+        self.addedclass = []
+        self.scrollingclasses = scrolled.ScrolledPanel(self.panel,size=(400,100))
+        self.classlistpos = self.scrollingclasses.GetPosition()
+        self.scrollingclasses.SetAutoLayout(1)
+        self.scrollingclasses.SetupScrolling()
+        self.scrollsizer = wx.BoxSizer(wx.VERTICAL)
         if existingfile:
             try:
                 for i,row in enumerate(savedata):
                     classname = row.split(',')[0]
                     target = row.split(',')[1]
                     date = row.split(',')[2]
-                    if i == 0:
-                        Header = wx.StaticText(panel, -1, style = wx.ALIGN_LEFT)
-                        Header.SetFont(Header_font.Underlined())
-                        Header.SetLabel(' Class\t Target Score  \tExam Date\n')
-                        menu_sizer.Add(Header, 0, wx.RIGHT | wx.LEFT, 20)
                     # Grades = wx.StaticText(panel, -1, style = wx.ALIGN_LEFT)
                     # Grades.SetFont(Header_font)
                     inputs = ' ' + classname + ':\t' + target + '% \t' + date + '\n'
-                    self.cb = wx.CheckBox(panel, label=inputs)
-                    menu_sizer.Add(self.cb)
-
+                    self.cb = wx.CheckBox(self.scrollingclasses, label=inputs)#self.cb = wx.CheckBox(panel, label=inputs)
+                    self.Bind(wx.EVT_CHECKBOX,self.ifChecked)
+                    self.scrollsizer.Add(self.cb, 0, wx.ALL | wx.RIGHT, 0)#menu_sizer.Add(self.cb)
+                self.scrollingclasses.SetSizer(self.scrollsizer)
+                self.scrollingclasses.Layout()
+                self.classlistpos = self.sizer.GetPosition()
+                self.sizer.Add(self.scrollingclasses, 1, wx.EXPAND)
             except: pass
-        else: pass
+        else:
+            self.cb = wx.StaticText('No classes added yet.')
+            self.scrollsizer.Add(self.cb, 0, wx.ALL | wx.RIGHT, 0)#menu_sizer.Add(self.cb)
+            self.scrollingclasses.SetSizer(self.scrollsizer)
+            self.scrollingclasses.Layout()
+            self.classlistpos = self.sizer.GetPosition()
+            self.sizer.Add(self.scrollingclasses, 1, wx.EXPAND)
 
         #buttons UI
-        start_button = wx.Button(panel, label='Add Class') # button to add class (calls self.start_press function)
+        start_button = wx.Button(self.panel, label='Delete selected') # button to add class (calls self.start_press function)
+        start_button.Bind(wx.EVT_BUTTON, self.deleteselected)
+        self.sizer.Add(start_button, 0, wx.UP | wx.LEFT, 20)
+        self.sizer.AddSpacer(5)
+
+        start_button = wx.Button(self.panel, label='Edit selected') # button to add class (calls self.start_press function)
+        start_button.Bind(wx.EVT_BUTTON, self.editselected)
+        self.sizer.Add(start_button, 0, wx.LEFT, 20)
+        self.sizer.AddSpacer(5)
+
+        start_button = wx.Button(self.panel, label='Add Class') # button to add class (calls self.start_press function)
         start_button.Bind(wx.EVT_BUTTON, self.start_press)
-        menu_sizer.Add(start_button, 0, wx.UP | wx.LEFT, 20)
-        menu_sizer.AddSpacer(5)
+        self.sizer.Add(start_button, 0, wx.LEFT, 20)
+        self.sizer.AddSpacer(5)
 
-        quit_button = wx.Button(panel, label='Quit') # button to quit the program
+        quit_button = wx.Button(self.panel, label='Quit') # button to quit the program
         quit_button.Bind(wx.EVT_BUTTON, self.QuitAll)
-        menu_sizer.Add(quit_button, 0, wx.DOWN | wx.LEFT, 20)
-        menu_sizer.AddSpacer(15)
+        self.sizer.Add(quit_button, 0, wx.DOWN | wx.LEFT, 20)
+        self.sizer.AddSpacer(5)
 
-        menu_sizer.Add((0,0), 1, wx.EXPAND)
-        shoma_statement = wx.StaticText(panel, -1, style = wx.ALIGN_CENTRE)
+        #menu_sizer.Add((0,0), 1, wx.EXPAND)
+        shoma_statement = wx.StaticText(self.panel, -1, style = wx.ALIGN_CENTRE)
         shoma_statement_font = wx.Font(12, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
         shoma_statement.SetFont(shoma_statement_font)
         shoma_statement.SetLabel('Version '+version + '\n' + DEVELOPED_BY_SHOMA)
-        menu_sizer.Add(shoma_statement,1,wx.UP | wx.CENTER,0)
-        panel.SetSizer(menu_sizer)
+        self.sizer.Add(shoma_statement,0,wx.BOTTOM | wx.CENTER | wx.EXPAND,0)
+        self.panel.SetSizer(self.sizer)
         self.Show()
 
     def menuquit(self,Event):
@@ -120,11 +147,87 @@ class MainMenu(wx.Frame):
     def MacPrintFile(self, file_path):
         pass
 
+    # Checkbox to select for deletion/edit
+    def ifChecked(self, event):
+        cb = event.GetEventObject()
+        if cb.GetValue():
+            classname = cb.GetLabel().split(':')[0][1:] # get the name of class from label 
+            self.deleteoredit.append(classname)
+
+    def deleteselected(self, event):
+        if len(self.deleteoredit) > 0:
+            print('call function to delete: ',self.deleteoredit) # need to write function to delete/edit selected class
+            wx.Yield()
+        else:
+            WarningPopup('Nothing selected!','Please select the classes you wish to delete.',(450,100),self)
+
+    def editselected(self, event):
+        if len(self.deleteoredit) > 0:
+            print('call function to edit: ',self.deleteoredit) # need to write function to delete/edit selected class
+            wx.Yield()
+        else:
+            WarningPopup('Nothing selected!','Please select the classes you wish to edit.',(450,100),self)
+
     def start_press(self, event): # add class, calls AddClassWindow, which opens a pop up
         #inputpath = self.text_ctrl.GetValue()
         classinfo = AddClassWindow(self)  
-        print(classinfo)
-        return classinfo 
+
+    def newclassreload(self): # function called by OTHER frames to reload this page
+        print('(test) Added class is: ',self.addedclass)
+        """
+        self.panel = wx.Panel(self)
+        self.sizer  = wx.BoxSizer(wx.VERTICAL)
+
+        #top left design
+        shoma_icon = wx.StaticBitmap(self.panel, bitmap=wx.Bitmap('./images/Easy_A_logo_mini.png'),style = wx.ALIGN_LEFT)
+        self.sizer.Add(shoma_icon, 0, wx.RIGHT | wx.LEFT, 20)
+        #menu_sizer.AddSpacer(0)
+        Header_font = wx.Font(14, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        Welcome = wx.StaticText(self.panel, -1, style = wx.ALIGN_LEFT)
+        Welcome.SetLabel('Easy A\nMain Menu\n\n')
+        Welcome.SetFont(Header_font)
+        self.sizer.Add(Welcome, 0, wx.RIGHT | wx.LEFT, 20)
+        """
+        self.scrollingclasses.Destroy()
+        self.scrollingclasses2 = scrolled.ScrolledPanel(self.panel,pos=self.classlistpos,size=(400,100)) # pos=self.classlistpos
+        self.scrollingclasses2.SetAutoLayout(1)
+        self.scrollingclasses2.SetupScrolling()
+        self.scrollsizer2 = wx.BoxSizer(wx.VERTICAL)
+        if existingfile:
+            for i,row in enumerate(savedata):
+                classname = row.split(',')[0]
+                target = row.split(',')[1]
+                date = row.split(',')[2]
+                # Grades = wx.StaticText(panel, -1, style = wx.ALIGN_LEFT)
+                # Grades.SetFont(Header_font)
+                inputs = ' ' + classname + ':\t' + target + '% \t' + date + '\n'
+                self.cb = wx.CheckBox(self.scrollingclasses2, label=inputs)#self.cb = wx.CheckBox(panel, label=inputs)
+                self.Bind(wx.EVT_CHECKBOX,self.ifChecked)
+                self.scrollsizer2.Add(self.cb, 0, wx.ALL | wx.RIGHT, 0)#menu_sizer.Add(self.cb)
+            inputs = ' ' + self.addedclass[0] + ':\t' + self.addedclass[1] + '% \t' + self.addedclass[2] + '\n'
+            self.cb = wx.CheckBox(self.scrollingclasses2, label=inputs)#self.cb = wx.CheckBox(panel, label=inputs)
+            self.Bind(wx.EVT_CHECKBOX,self.ifChecked)
+            self.scrollsizer2.Add(self.cb, 0, wx.ALL | wx.RIGHT, 0)#menu_sizer.Add(self.cb) 
+            self.scrollingclasses2.SetSizer(self.scrollsizer2)
+            self.scrollingclasses2.Layout()
+        else:
+            inputs = ' ' + self.addedclass[0] + ':\t' + self.addedclass[1] + '% \t' + self.addedclass[2] + '\n'
+            self.cb = wx.CheckBox(self.scrollingclasses2, label=inputs)#self.cb = wx.CheckBox(panel, label=inputs)
+            self.Bind(wx.EVT_CHECKBOX,self.ifChecked)
+            self.scrollsizer2.Add(self.cb, 0, wx.ALL | wx.RIGHT, 0)#menu_sizer.Add(self.cb) 
+            self.scrollingclasses2.SetSizer(self.scrollsizer2)
+            self.scrollingclasses2.Layout()
+        self.scrollingclasses2.Refresh()
+        self.scrollingclasses2.Update()
+        self.scrollingclasses2.Show()
+
+        #self.sizer.Replace(self.scrollingclasses, self.scrollingclasses2)
+        self.sizer.Add(self.scrollingclasses2, 1, wx.EXPAND)
+
+        self.Refresh()
+        self.Update()
+        self.Layout()
+        self.Show()
 
     def QuitAll(self, event):
         wx.Exit()
@@ -161,6 +264,7 @@ def AddClassWindow(parentframe):
     :type parentframe: wx.Frame
     :rtype: dict classinfo (False if closed)
     """
+    parent = parentframe
     class AddClassWindow(wx.Frame):
         def __init__(self):
             super(AddClassWindow, self).__init__(parent=parentframe, title='Adding a class',size=(350, 800))#(400, 400)
@@ -173,52 +277,38 @@ def AddClassWindow(parentframe):
             window_size.Add(Name, 0, wx.UP | wx.CENTER, 15)
             self.text_ctrl = wx.TextCtrl(panel)
             window_size.Add(self.text_ctrl, 0, wx.UP | wx.CENTER, 15)
-            classname = self.text_ctrl.GetValue()
-            self.classinfo['classname'] = classname
 
             Quiz = wx.StaticText(panel, -1, style = wx.ALIGN_CENTRE)
             Quiz.SetLabel('Quiz weight (0-100%) and your score')
             window_size.Add(Quiz, 0, wx.UP | wx.CENTER, 15)
             self.text_ctrl1 = wx.TextCtrl(panel)
-            window_size.Add(self.text_ctrl1, 0, wx.UP | wx.CENTER, 15)
-            quizweight = self.text_ctrl1.GetValue()
             self.text_ctrl11 = wx.TextCtrl(panel)
+            window_size.Add(self.text_ctrl1, 0, wx.UP | wx.CENTER, 15)
             window_size.Add(self.text_ctrl11, 0, wx.UP | wx.CENTER, 15)
-            quizscore = self.text_ctrl11.GetValue()
-            self.classinfo['quiz'] = (quizweight,quizscore)
 
             Assignment = wx.StaticText(panel, -1, style = wx.ALIGN_CENTRE)
             Assignment.SetLabel('Assignment weight (0-100%) and your score')
             window_size.Add(Assignment, 0, wx.UP | wx.CENTER, 15)
             self.text_ctrl2 = wx.TextCtrl(panel)
-            window_size.Add(self.text_ctrl2, 0, wx.UP | wx.CENTER, 15)
-            Assignmentweight = self.text_ctrl2.GetValue()
             self.text_ctrl21 = wx.TextCtrl(panel)
+            window_size.Add(self.text_ctrl2, 0, wx.UP | wx.CENTER, 15)
             window_size.Add(self.text_ctrl21, 0, wx.UP | wx.CENTER, 15)
-            Assignmentscore = self.text_ctrl21.GetValue()
-            self.classinfo['Assignment'] = (Assignmentweight,Assignmentscore)
 
             Midterm = wx.StaticText(panel, -1, style = wx.ALIGN_CENTRE)
             Midterm.SetLabel('Midterm weight (0-100%) and your score')
             window_size.Add(Midterm, 0, wx.UP | wx.CENTER, 15)
             self.text_ctrl3 = wx.TextCtrl(panel)
-            window_size.Add(self.text_ctrl3, 0, wx.UP | wx.CENTER, 15)
-            Midtermweight = self.text_ctrl3.GetValue()
             self.text_ctrl31 = wx.TextCtrl(panel)
+            window_size.Add(self.text_ctrl3, 0, wx.UP | wx.CENTER, 15)
             window_size.Add(self.text_ctrl31, 0, wx.UP | wx.CENTER, 15)
-            Midtermscore = self.text_ctrl31.GetValue()
-            self.classinfo['Assignment'] = (Assignmentweight,Assignmentscore)
 
             Target = wx.StaticText(panel, -1, style=wx.ALIGN_CENTRE)
             Target.SetLabel('Final exam weight (0-100%) and your target course %')
             window_size.Add(Target, 0, wx.UP | wx.CENTER, 15)
             self.text_ctrl4 = wx.TextCtrl(panel)
-            window_size.Add(self.text_ctrl4, 0, wx.UP | wx.CENTER, 15)
-            Finalweight = self.text_ctrl4.GetValue()
             self.text_ctrl41 = wx.TextCtrl(panel)
+            window_size.Add(self.text_ctrl4, 0, wx.UP | wx.CENTER, 15)
             window_size.Add(self.text_ctrl41, 0, wx.UP | wx.CENTER, 15)
-            Targetscore = self.text_ctrl41.GetValue()
-            self.classinfo['Target'] = (Finalweight, Targetscore)
 
             ExamDate = wx.StaticText(panel, -1, style=wx.ALIGN_CENTRE)
             ExamDate.SetLabel('Exam date')
@@ -231,7 +321,7 @@ def AddClassWindow(parentframe):
             add_button.Bind(wx.EVT_BUTTON, self.addpress)
             window_size.Add(add_button, 0, wx.UP | wx.CENTER, 0)
             panel.SetSizer(window_size)
-            window_size.AddSpacer(30)
+            window_size.AddSpacer(5)
 
             close_button = wx.Button(panel, label='Close')
             close_button.Bind(wx.EVT_BUTTON, self.closepress)
@@ -244,13 +334,32 @@ def AddClassWindow(parentframe):
             return False
 
         def addpress(self, event):
+            classname = self.text_ctrl.GetValue()
+            self.classinfo['classname'] = classname 
+
+            quizweight = self.text_ctrl1.GetValue()
+            quizscore = self.text_ctrl11.GetValue()
+            self.classinfo['quiz'] = (quizweight,quizscore)
+
+            Assignmentweight = self.text_ctrl2.GetValue()
+            Assignmentscore = self.text_ctrl21.GetValue()
+            self.classinfo['Assignment'] = (Assignmentweight,Assignmentscore)
+
+            Midtermweight = self.text_ctrl3.GetValue()
+            Midtermscore = self.text_ctrl31.GetValue()
+            self.classinfo['Assignment'] = (Midtermweight,Midtermscore)
+
+            Finalweight = self.text_ctrl4.GetValue()
+            Targetscore = self.text_ctrl41.GetValue()
+            self.classinfo['Target'] = (Finalweight, Targetscore)
+
             date = self.text_ctrl5.GetValue()
             self.classinfo['Date'] = date.Format('%Y/%m/%d')
-            #print self.classinfo['Date']
-            self.Close()
-            return self.classinfo
+            #parent.addedclass = self.classinfo
+            #print(self.classinfo)
+
             turn = 0
-            for i, j in classinfo.items():
+            for i, j in self.classinfo.items():
                 if isinstance(j, str) != True:
                     if i != 'Target':
                         if turn == 0:
@@ -268,9 +377,24 @@ def AddClassWindow(parentframe):
                     if i == 'Date':
                     	exam_date = j
 
-            return mark_reqd
-            return my_course
-            return exam_date
+            my_course = my_course.upper() # make classname uppercase
+            parent.addedclass = [my_course,str(int(mark_reqd)),exam_date]
+            parent.newclassreload()
+            """
+            if os.path.isfile('./save/saved.text'): # open existing save file
+                saved = open('./save/saved.text','a+')
+            else:
+                saved = open('./save/saved.text','w+') # open new file
+            #saved.write(my_course + ',' + '{:.1f}'.format(mark_reqd) + ',' + exam_date + '\n') # if writing as float (1 decimal prec)
+            saved.write(my_course + ',' + str(int(mark_reqd)) + ',' + exam_date + '\n') # if writing as float (1 decimal prec)
+            saved.close()
+            """
+            #print(my_course,mark_reqd,exam_date)
+            #print(my_course,str(int(mark_reqd)),exam_date)
+            self.Close()
+            #return mark_reqd
+            #return my_course
+            #return exam_date
 
 
 
